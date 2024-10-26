d573ca013818d36cde9d21c0d438b4b722739a06
hoethan231 <Icydragons1357@gmail.com>
2024-10-26 16:56:42 -0700

    add button onto homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { FlipWords } from "@/components/ui/flip-words";
 import { Highlight } from "@/components/ui/hero-highlight";
 import { TracingBeam } from "@/components/ui/tracing-beam";
@@ -9,17 +10,18 @@ export default function Home() {
   return (
     <TracingBeam>
       <div className="min-h-screen flex justify-center items-center">
-        <div className="flex flex-col text-center mx-auto ">
-          <div className="text-2xl px-4 md:text-4xl lg:text-8xl font-bold text-white max-w-4xl leading-relaxed lg:leading-snug">
-            <h1>Welcome To</h1>
+        <div className="flex flex-col text-center mx-auto items-center">
+          <div className="text-3xl px-4 md:text-4xl lg:text-8xl font-bold text-white max-w-4xl leading-relaxed lg:leading-snug">
+            <h1 className="md:p-3">Welcome To</h1>
             <Highlight className="text-[#0E0317]">
               GROOVEBOX
             </Highlight>
           </div>
-          <div className="text-2xl text-white py-6">
+          <div className="lg:text-2xl md:text-lg s:text-m text-white py-6">
             <div>Your go-to destination for discovering and listening to the latest hits!</div>
             <div>Get started to jump right into<FlipWords words={genres} className="text-[#c2a2e9] font-bold"/></div>
           </div>
+          <Button variant="outline" className="bg-white w-[50%]">Get Started!</Button>
         </div>
       </div>
     </TracingBeam>
