14985c6dd6ca0fcf5a367b96cddf4e64a42e7be6
Ethan <118030852+hoethan231@users.noreply.github.com>
2024-11-08 17:10:35 -0800

    Page/playlist (#4)
    * implement playlist page
    
    * add scrolling on table
    
    * add song filtering

---
+++ frontend/app/library/playlist/page.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import uni from "@/public/image.webp";
+import Image from 'next/image';
+import { DataTable } from "@/components/ui/data-table";
+import { columns } from '@/components/ui/columns';
+
+const PlaylistPage: React.FC = () => {
+
+    const tempSongs = [
+        {
+            idx:"1",
+            title:"fouroclock",
+            album:"awesome",
+            date:"october",
+            length:"1:30"
+        },
+        {
+            idx:"2",
+            title:"cats",
+            album:"awesomer",
+            date:"november",
+            length:"2:30"
+        },
+        {
+            idx:"3",
+            title:"trees",
+            album:"disaster",
+            date:"frebuaray",
+            length:"1:33"
+        },
+    ];
+
+    return (
+        <div className='flex min-h-screen min-w-screen bg-gradient-to-b from-[#180F18] to-[#1D1D20]'>
+            <div className="min-h-screen w-1/3 flex justify-center items-center">
+                <div className="text-white w-[80%] flex justify-center items-center flex-col">
+                    <Image src={uni} alt="" className='rounded-[10%]' />
+                    <h1 className='p-3 text-[2.5vw] font-bold'>Meow Playlist!!</h1>
+                    <h2 className='text-[1vw]'>Gymming music!!</h2>
+                </div>
+            </div>
+            <div className="max-h-screen w-2/3 flex justify-center items-center">
+                <DataTable columns={columns} data={tempSongs} />
+            </div>
+        </div>
+    );
+};
+
+export default PlaylistPage;
---
+++ frontend/components/ui/data-table.tsx
@@ -0,0 +1,121 @@
+"use client"
+
+import * as React from "react";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+  SortingState,
+  getSortedRowModel,
+  ColumnFiltersState,
+  getFilteredRowModel,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Input } from "@/components/ui/input";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
+  const [rowSelection, setRowSelection] = React.useState({});
+  const [pagination, setPagination] = React.useState({
+    pageIndex: 0,
+    pageSize: 999999,
+  });
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      pagination,
+      columnFilters,
+      rowSelection,
+    },
+  })
+
+  return (
+    <div className="h-full w-full flex justify-center flex-col mr-[6vw]">
+      <div className="flex items-center py-4">
+          <Input
+            placeholder="Filter songs..."
+            value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}
+            onChange={(event: any) =>
+              table.getColumn("title")?.setFilterValue(event.target.value)
+            }
+            className="max-w-sm text-white"
+          />
+      </div>
+      <div className="w-full h-[70%] flex">
+        <div className="text-white bg-[#898989]/[.20] rounded-2xl border-[#747474] border-2 w-full overflow-y-auto no-scrollbar">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    )
+                  })}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={columns.length} className="h-24 text-center">
+                    Start by Adding Songs to your Playlist!
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  )
+}
