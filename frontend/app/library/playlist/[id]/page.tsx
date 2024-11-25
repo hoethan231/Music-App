66eb2cb15e4e9cc11398c9cec2128da4fefb212e
TrevorChong <trevor.chong6@gmail.com>
2024-11-25 04:41:08 +0000

    implement playlist

--- frontend/app/library/playlist/[id]/page.tsx
+++ frontend/app/library/playlist/[id]/page.tsx
@@ -6,12 +6,18 @@ import { DataTable } from "@/components/ui/data-table";
 import { columns } from '@/components/ui/columns';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from "@/app/firebase/config";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { useRouter } from 'next/navigation';
 
 interface PlaylistPageProps {
+    createdAt: string;
+    description: string;
+    img: string;
+    name: string;
+    songs: string[];
+    uid: string;
     params: { id: string };
-}
+}[]
 
 const PlaylistPage: React.FC<PlaylistPageProps> = ({ params }) => {
     const token = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
@@ -30,11 +36,11 @@ const PlaylistPage: React.FC<PlaylistPageProps> = ({ params }) => {
     const router = useRouter();
     const [user, loading, error] = useAuthState(auth);
     const [fetching, setFetching] = useState(true);
-    
+
     // if (!user) {
     //     router.push("/login");
     // }
-        
+
     const [album, setAlbum] = useState<{ img: string, name: string, description: string, background: string }>({ img: "", name: "", description: "", background: "" });
     const [tracks, setTracks] = useState<any[]>([]);
 
@@ -53,42 +59,88 @@ const PlaylistPage: React.FC<PlaylistPageProps> = ({ params }) => {
                 };
                 setAlbum(albumData);
                 const formattedTracks = data.tracks.items
-                .filter((item: any) => item.track.type === "track")
-                .map((item: any, index: number) => ({
-                    idx: index + 1,
-                    id: item.track.id,
-                    title: item.track.name,
-                    artist: item.track.artists[0].name,
-                    album: item.track.album.name,
-                    img: item.track.album.images[1].url,
-                    date: item.added_at.substring(0,10),
-                    length: new Date(item.track.duration_ms).toISOString(),
-                }));
+                    .filter((item: any) => item.track.type === "track")
+                    .map((item: any, index: number) => ({
+                        idx: index + 1,
+                        id: item.track.id,
+                        title: item.track.name,
+                        artist: item.track.artists[0].name,
+                        album: item.track.album.name,
+                        img: item.track.album.images[1].url,
+                        date: item.added_at.substring(0, 10),
+                        length: new Date(item.track.duration_ms).toISOString(),
+                    }));
                 setTracks(formattedTracks);
                 setFetching(false);
             });
     };
 
-    const fetchLikedTracks = async () => {
+    const fetchLikedTracks = async (idx: number) => {
         if (user?.uid) {
             await getDoc(doc(db, 'users', user.uid))
                 .then((doc) => {
                     if (doc.exists()) {
-                        setAlbum(doc.data().playlists[1]);
-                        setTracks(doc.data().playlists[1].songs);
+                        setAlbum(doc.data().playlists[idx]);
+                        setTracks(doc.data().playlists[idx]?.songs || []);
                     }
                     setFetching(false);
                 });
         }
     };
 
+    const fetchUserPlaylists = async (userID: string) => {
+        const userDoc = await getDoc(doc(db, 'users', userID));
+        if (userDoc.exists()) {
+            const userData = userDoc.data();
+            return userData.playlists || [];
+        }
+        return [];
+    };
+
+    const getIdx = async () => {
+        if (user) {
+            const playlists = await fetchUserPlaylists(user.uid);
+            return playlists.findIndex((playlist: PlaylistPageProps) => playlist.name === album.name);
+        }
+        return 1;
+    }
+
+    const createPlaylist = async () => {
+        if (user?.uid) {
+            const playlists = await fetchUserPlaylists(user.uid);
+            const newPlaylist = {
+                createdAt: new Date().toISOString(),
+                description: "New Playlist",
+                img: "https://firebasestorage.googleapis.com/v0/b/music-app-db471.firebasestorage.app/o/default-playlist.jpg?alt=media&token=4c81b791-83d2-4855-b39e-cb4e7799f3e8",
+                name: `Playlist ${playlists.length - 1}`,
+                songs: [],
+                uid: user.uid,
+                background: "-1",
+            }
+            setAlbum(newPlaylist);
+            try {
+                await updateDoc(doc(db, 'users', user.uid), {
+                    playlists: [...playlists, newPlaylist],
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    };
+
     useEffect(() => {
         if (!loading && user) {
             if (id === 'liked-songs') {
-                fetchLikedTracks();
+                fetchLikedTracks(1);
+            }
+            else if (id === "add-playlist") {
+                createPlaylist();
+            }
+            else if (id.length === 22) {
+                fetchSpotify();
             }
             else {
-                fetchSpotify();
+                getIdx().then(idx => fetchLikedTracks(idx));
             }
         }
     }, [user, loading, error]);
@@ -104,7 +156,7 @@ const PlaylistPage: React.FC<PlaylistPageProps> = ({ params }) => {
                             <Skeleton className='w-[14vw] h-[2vh]' />
                         </>
                     ) : (
-                        album.background ? (
+                        album.background !== "-1" ? (
                             <>
                                 <Card style={{ background: album.background }}>
                                     <CardContent className="flex w-[20vw] h-[20vw] rounded-[10%] items-center justify-center p-6 bg-[#747474] bg-opacity-20 text-center">
@@ -128,7 +180,7 @@ const PlaylistPage: React.FC<PlaylistPageProps> = ({ params }) => {
                 </div>
             </div>
             <div className="max-h-screen w-2/3 flex justify-center items-center">
-                <DataTable columns={columns} data={tracks} fetching={fetching}/>
+                <DataTable columns={columns} data={tracks} fetching={fetching} />
             </div>
         </div>
     );
